eea2639500e395f256ac87c206663a88a9dbdd1d
Artur Khamidulliun <artrimskiy@gmail.com>
2024-03-03 23:31:16 +0500

    create classmethod cast_to_object_list

--- src/work_with_vacancies.py
+++ src/work_with_vacancies.py
@@ -9,3 +9,21 @@ class Vacancies:
         self.url = url
         self.salary_from = salary_from
         self.city = city
+
+    @classmethod
+    def cast_to_object_list(cls, hh_vacancies):
+        """
+        Преобразовывает список вакансий в список экземпляров класса
+        """
+        vacancies = []
+        for vac_data in hh_vacancies['items']:
+            title = vac_data["name"]
+            url = vac_data["alternate_url"]
+            if vac_data['salary']['from'] is None:
+                vac_data['salary']['from'] = 0
+            salary_from = vac_data["salary"]["from"]
+            city = vac_data["area"]['name']
+            # cls(title, url, salary_from, city)
+            instance = cls(title, url, salary_from, city)
+            vacancies.append(instance)
+        return vacancies
