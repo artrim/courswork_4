e2485d5f8133f1737ada687debe2c10f7e2e2b24
Artur Khamidulliun <artrimskiy@gmail.com>
2024-03-03 23:14:42 +0500

    create class Vacancies

---
+++ src/work_with_vacancies.py
@@ -0,0 +1,11 @@
+class Vacancies:
+    """
+    Класс для работы с вакансиями
+    """
+
+    def __init__(self, title, url, salary_from, city):
+
+        self.title = title
+        self.url = url
+        self.salary_from = salary_from
+        self.city = city
