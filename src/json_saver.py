671e10130550d793aa5a447ec08281aa077b0069
Artur Khamidulliun <artrimskiy@gmail.com>
2024-03-25 22:08:04 +0500

    update json_server

--- src/json_saver.py
+++ src/json_saver.py
@@ -12,14 +12,23 @@ class JSONSaver(BaseSaver):
         Сохраняет вакансии в json файл
         """
         vacancy_list = []
-        for i in vacancies:
+        if isinstance(vacancies, list):
+            for i in vacancies:
+                vacancy_list.append({
+                    'name': i.title,
+                    'city': i.city,
+                    'salary': i.salary_from,
+                    'url': i.url})
+            with open('../data/vacancy.json', 'w') as f:
+                json.dump(vacancy_list, f, indent=4, ensure_ascii=False)
+        else:
             vacancy_list.append({
-                'name': i.title,
-                'city': i.city,
-                'salary': i.salary_from,
-                'url': i.url})
-        with open('../data/vacancy.json', 'w') as f:
-            json.dump(vacancy_list, f, indent=4, ensure_ascii=False)
+                'name': vacancies.title,
+                'city': vacancies.city,
+                'salary': vacancies.salary_from,
+                'url': vacancies.url})
+            with open('../data/vacancy.json', 'w') as f:
+                json.dump(vacancy_list, f, indent=4, ensure_ascii=False)
 
     def delete_vacancy(self):
         pass
